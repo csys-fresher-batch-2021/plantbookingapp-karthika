a1157026639a45d2d1adaad83ea07fa0ada0f0e7
Karthika C <karthikaachairman@gmail.com>
2021-05-19 12:14:48 +0530

    feature2 added

--- src/main/java/in/karthika/servlet/adminloginservlet.java
+++ src/main/java/in/karthika/servlet/adminloginservlet.java
@@ -22,7 +22,7 @@ public class AdminLoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	@Override
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String userId = request.getParameter("userId");
 		String password = request.getParameter("password");
