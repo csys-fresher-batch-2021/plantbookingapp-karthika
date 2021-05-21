48f450cbc9ee2d3275629fb96786fd32ff83f4df
Karthika C <karthikaachairman@gmail.com>
2021-05-21 19:31:57 +0530

    Modified feature5 add

--- src/main/java/in/karthika/service/CartService.java
+++ src/main/java/in/karthika/service/CartService.java
@@ -1,7 +1,9 @@
 package in.karthika.service;
 
 import in.karthika.dao.CartData;
+import in.karthika.dao.PlantData;
 import in.karthika.model.Cart;
+import in.karthika.model.Plant;
 
 public class CartService {
 
@@ -21,18 +23,35 @@ public class CartService {
 		boolean isAdd = false;
 		boolean exist = false;
 		double price = 0;
-		for (Cart add : CartData.getCart()) {
+		for (Plant add : PlantData.getPlants()) {
 			if (add.getPlantName().equalsIgnoreCase(plantName.trim())) {
 				price = add.getPrice();
-				exist = true;
 				break;
 			}
 		}
+		exist = checkCart(plantName);
 		if (!exist) {
 			CartData.addCart(plantName, price);
 			isAdd = true;
 		}
 		return isAdd;
+	}
+	
+	/**
+	 * This method is used to check whether the plant is already present in the cart or not 
+	 * @param plantName
+	 * @return
+	 */
+	public static boolean checkCart(String plantName) {
+		boolean check=false;
+		for (Cart add : CartData.getCart()) {
+			if (add.getPlantName().equalsIgnoreCase(plantName.trim())) {
+				check= true;
+				break;
+			}
+		}
+		return check;
+		
 	}
 
 }
